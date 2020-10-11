9b93ec07c3e6898487b5514de8836a1e309586e3
Dreamwalker <aristojeff@gmail.com>
2020-10-11 17:59:52 +0900

    Create discovery_home_page.dart

---
+++ lib/ep601_game_discovery_app/discovery_home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DiscoveryHomePage extends StatefulWidget {
+  @override
+  _DiscoveryHomePageState createState() => _DiscoveryHomePageState();
+}
+
+class _DiscoveryHomePageState extends State<DiscoveryHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
