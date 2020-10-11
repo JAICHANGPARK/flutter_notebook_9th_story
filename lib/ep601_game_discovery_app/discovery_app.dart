96722ec440801fe1e84f17a9fca2ebda6a38992f
Dreamwalker <aristojeff@gmail.com>
2020-10-11 17:59:55 +0900

    Update discovery_app.dart

--- lib/ep601_game_discovery_app/discovery_app.dart
+++ lib/ep601_game_discovery_app/discovery_app.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'discovery_home_page.dart';
+
 class GameDiscoveryApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: GameDiscoveryHomePage(),
+      home: DiscoveryHomePage(),
     );
   }
 }
+
+
