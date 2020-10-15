68fb9d94f5d4df8a46055e867c426c6c96affb89
Dreamwalker <aristojeff@gmail.com>
2020-10-15 19:57:48 +0900

    Update task_management_home_page.dart

---
+++ lib/ep605_task_management_app/task_management_home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TaskManagementHomePage extends StatefulWidget {
+  @override
+  _TaskManagementHomePageState createState() => _TaskManagementHomePageState();
+}
+
+class _TaskManagementHomePageState extends State<TaskManagementHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
