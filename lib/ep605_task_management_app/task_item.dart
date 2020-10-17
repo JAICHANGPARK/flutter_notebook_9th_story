999b18a3f3d03ff144021f83d6efb84c7d4a64ca
Dreamwalker <aristojeff@gmail.com>
2020-10-17 21:44:18 +0900

    Update task_item.dart

--- lib/ep605_task_management_app/task_item.dart
+++ lib/ep605_task_management_app/task_item.dart
@@ -8,8 +8,10 @@ class TaskItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      elevation: index == 0 ? 8 : 4,
       shape: index == 0
           ? RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(4),
               side: BorderSide(
               color: Colors.grey,
             ))
