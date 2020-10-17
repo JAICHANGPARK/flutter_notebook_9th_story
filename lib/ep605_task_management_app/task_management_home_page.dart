b967e0336871164eb04f5b6e064311489b2ca538
Dreamwalker <aristojeff@gmail.com>
2020-10-17 21:43:26 +0900

    Update task_management_home_page.dart

--- lib/ep605_task_management_app/task_management_home_page.dart
+++ lib/ep605_task_management_app/task_management_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_9th_story/ep605_task_management_app/task_item.dart';
 
 class TaskManagementHomePage extends StatefulWidget {
   @override
@@ -119,88 +120,7 @@ class _TaskManagementHomePageState extends State<TaskManagementHomePage> {
                                     flex: 15,
                                     child: ListView.builder(
                                       itemBuilder: (context, index) {
-                                        return Card(
-                                          shape: index == 0
-                                              ? RoundedRectangleBorder(
-                                                  side: BorderSide(
-                                                  color: Colors.grey,
-                                                ))
-                                              : null,
-                                          margin: EdgeInsets.only(bottom: 16, left: 4, right: 4),
-                                          child: Padding(
-                                            padding: const EdgeInsets.all(12.0),
-                                            child: Column(
-                                              children: [
-                                                Row(
-                                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                                  children: [
-                                                    Text("Brands.Co Landing page\nIdea"),
-                                                    Text(
-                                                      "Due Today\n14 Ocd",
-                                                      style: TextStyle(color: Colors.deepOrange, fontSize: 12),
-                                                    )
-                                                  ],
-                                                ),
-                                                Padding(
-                                                  padding: const EdgeInsets.symmetric(vertical: 8),
-                                                  child: Row(
-                                                    children: [
-                                                      Container(
-                                                        child: Text(
-                                                          "HIGH",
-                                                          style: TextStyle(
-                                                              color: Colors.orange,
-                                                              fontSize: 12,
-                                                              fontWeight: FontWeight.bold),
-                                                        ),
-                                                        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 4),
-                                                        decoration: BoxDecoration(
-                                                          color: Colors.orange[50],
-                                                          borderRadius: BorderRadius.circular(2),
-                                                        ),
-                                                      ),
-                                                      SizedBox(
-                                                        width: 12,
-                                                      ),
-                                                      Container(
-                                                        child: Text(
-                                                          "DESIGN",
-                                                          style: TextStyle(
-                                                              color: Colors.teal,
-                                                              fontSize: 12,
-                                                              fontWeight: FontWeight.bold),
-                                                        ),
-                                                        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 4),
-                                                        decoration: BoxDecoration(
-                                                          color: Colors.teal[50],
-                                                          borderRadius: BorderRadius.circular(2),
-                                                        ),
-                                                      )
-                                                    ],
-                                                  ),
-                                                ),
-                                                Row(
-                                                  children: [
-                                                    Icon(
-                                                      Icons.attach_file_outlined,
-                                                      size: 16,
-                                                    ),
-                                                    Text("1"),
-                                                    Spacer(),
-                                                    Padding(
-                                                      padding: const EdgeInsets.all(8.0),
-                                                      child: Text(
-                                                        "Dreamwalker",
-                                                        style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold),
-                                                      ),
-                                                    ),
-                                                    CircleAvatar(),
-                                                  ],
-                                                )
-                                              ],
-                                            ),
-                                          ),
-                                        );
+                                        return TaskItem(index);
                                       },
                                     ),
                                   )
