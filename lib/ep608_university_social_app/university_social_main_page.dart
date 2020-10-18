2660d4419633217e6ed722bb6280ec28dbe17825
Dreamwalker <aristojeff@gmail.com>
2020-10-18 18:51:25 +0900

    Update university_social_main_page.dart

--- lib/ep608_university_social_app/university_social_main_page.dart
+++ lib/ep608_university_social_app/university_social_main_page.dart
@@ -9,10 +9,37 @@ class _UniversitySocialMainPageState extends State<UniversitySocialMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          
-        ],
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                height: 84,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.blueGrey[50],
+                      blurRadius: 2,
+                      spreadRadius: 2,
+                      offset: Offset(
+                        0, 4
+                      )
+                    )
+                  ]
+                ),
+                child: Row(
+                  children: [
+
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
