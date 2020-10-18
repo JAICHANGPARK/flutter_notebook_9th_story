97b9f6d055f899a5759e771bc2ba93f50622cd90
Dreamwalker <aristojeff@gmail.com>
2020-10-18 18:43:38 +0900

    Update university_social_main_page.dart

--- lib/ep608_university_social_app/university_social_main_page.dart
+++ lib/ep608_university_social_app/university_social_main_page.dart
@@ -1,2 +1,13 @@
 import 'package:flutter/material.dart';
 
+class UniversitySocialMainPage extends StatefulWidget {
+  @override
+  _UniversitySocialMainPageState createState() => _UniversitySocialMainPageState();
+}
+
+class _UniversitySocialMainPageState extends State<UniversitySocialMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
