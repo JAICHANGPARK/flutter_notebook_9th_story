7a6c5226c007baf118ddd25652e2f46e5312ba70
Dreamwalker <aristojeff@gmail.com>
2020-10-18 18:43:52 +0900

    Update university_social_app.dart

--- lib/ep608_university_social_app/university_social_app.dart
+++ lib/ep608_university_social_app/university_social_app.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'university_social_main_page.dart';
+
 class UniversitySocialApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      home: UniversitySocialMainPage(),
     );
   }
 }
