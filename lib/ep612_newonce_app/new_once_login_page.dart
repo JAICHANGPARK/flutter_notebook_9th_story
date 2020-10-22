56717a1969d5a1d0cd9b02d7500c6fa43ca69d81
Dreamwalker <aristojeff@gmail.com>
2020-10-22 20:01:55 +0900

    Update new_once_login_page.dart

--- lib/ep612_newonce_app/new_once_login_page.dart
+++ lib/ep612_newonce_app/new_once_login_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class NewOnceLoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      
+    );
   }
 }
