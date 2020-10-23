437e780a4e6d499cc35b879a74e4a912f0bb7c61
Dreamwalker <aristojeff@gmail.com>
2020-10-23 21:20:37 +0900

    Update new_once_login_page.dart

--- lib/ep612_newonce_app/new_once_login_page.dart
+++ lib/ep612_newonce_app/new_once_login_page.dart
@@ -52,7 +52,11 @@ class NewOnceLoginPage extends StatelessWidget {
             ),
             Expanded(
               flex: 2,
-              child: Placeholder(),
+              child: Column(
+                children: [
+                  Text("Zaloz konto za pomoca")
+                ],
+              )
             ),
             Expanded(
               flex: 5,
