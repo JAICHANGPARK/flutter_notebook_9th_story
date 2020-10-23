fb2f25410a54d9e0a16e7f2dda0063c346157ff9
Dreamwalker <aristojeff@gmail.com>
2020-10-23 21:23:28 +0900

    Update new_once_login_page.dart

--- lib/ep612_newonce_app/new_once_login_page.dart
+++ lib/ep612_newonce_app/new_once_login_page.dart
@@ -12,37 +12,25 @@ class NewOnceLoginPage extends StatelessWidget {
               flex: 2,
               child: Row(
                 children: [
-                  Expanded(
-                      child: SizedBox()
-                  ),
+                  Expanded(child: SizedBox()),
                   Expanded(
                       child: Center(
-                        child: Text("newonce", style: GoogleFonts.abrilFatface(
-                            color: Colors.blueAccent,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 24
-                        ),
-                        ),
-                      )
-                  ),
+                    child: Text(
+                      "newonce",
+                      style:
+                          GoogleFonts.abrilFatface(color: Colors.blueAccent, fontWeight: FontWeight.bold, fontSize: 24),
+                    ),
+                  )),
                   Expanded(
                     child: Container(
                       margin: EdgeInsets.only(left: 48, bottom: 16, top: 16, right: 24),
                       decoration: BoxDecoration(
                           color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                                color: Colors.black,
-                                spreadRadius: 2,
-                                offset: Offset(2, 2)
-                            )
-                          ]
-                      ),
+                          boxShadow: [BoxShadow(color: Colors.black, spreadRadius: 2, offset: Offset(2, 2))]),
                       child: Center(
                         child: Icon(Icons.clear),
                       ),
                     ),
-
                   )
                 ],
               ),
@@ -52,38 +40,36 @@ class NewOnceLoginPage extends StatelessWidget {
               child: Container(),
             ),
             Expanded(
-              flex: 2,
-              child: Column(
-                children: [
-                  Expanded(
-                    flex: 1,
-                      child: Text("Zaloz konto za pomoca",
-                          , style: GoogleFonts.abrilFatface(
-                          color: Colors.blueAccent,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 24
-                      ),)),
-                  Expanded(
-                    flex: 2,
-                    child: Container(
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: Placeholder(),
-                          ),
-                          Expanded(
-                            child: Placeholder(),
-                          ),
-                          Expanded(
-                            child: Placeholder(),
-                          )
-                        ],
+                flex: 2,
+                child: Column(
+                  children: [
+                    Expanded(
+                        flex: 1,
+                        child: Text(
+                          "Zaloz konto za pomoca",
+                          style: GoogleFonts.abrilFatface(
+                              color: Colors.blueAccent, fontWeight: FontWeight.bold, fontSize: 24),
+                        )),
+                    Expanded(
+                      flex: 2,
+                      child: Container(
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Placeholder(),
+                            ),
+                            Expanded(
+                              child: Placeholder(),
+                            ),
+                            Expanded(
+                              child: Placeholder(),
+                            )
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              )
-            ),
+                  ],
+                )),
             Expanded(
               flex: 5,
               child: Placeholder(),
