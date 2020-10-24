f1b9476d85f278a92f360a1ceeee9f7ef473ceb9
Dreamwalker <aristojeff@gmail.com>
2020-10-24 18:05:28 +0900

    Update new_once_login_page.dart

--- lib/ep612_newonce_app/new_once_login_page.dart
+++ lib/ep612_newonce_app/new_once_login_page.dart
@@ -12,6 +12,7 @@ class _NewOnceLoginPageState extends State<NewOnceLoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Column(
           children: [
