99ebcd9d1847cf31bf255c011d9f3ed894d6f2fe
Dreamwalker <aristojeff@gmail.com>
2020-10-24 18:07:31 +0900

    Update new_once_main_page.dart

--- lib/ep612_newonce_app/new_once_main_page.dart
+++ lib/ep612_newonce_app/new_once_main_page.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
 
+class NewonceMainPage extends StatefulWidget {
+  @override
+  _NewonceMainPageState createState() => _NewonceMainPageState();
+}
+
+class _NewonceMainPageState extends State<NewonceMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
