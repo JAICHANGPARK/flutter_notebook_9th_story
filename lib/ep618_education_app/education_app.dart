ddfb2185e9a6322aeb5605696362f5e6835a6769
Dreamwalker <aristojeff@gmail.com>
2020-10-28 20:14:49 +0900

    Update education_app.dart

--- lib/ep618_education_app/education_app.dart
+++ lib/ep618_education_app/education_app.dart
@@ -4,6 +4,6 @@ import 'package:flutter/material.dart';
 class EducationApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp();
   }
 }
