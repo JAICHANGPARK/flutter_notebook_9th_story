caab0b1ff3c23c194c61f129bc7fffef4ff01b32
Dreamwalker <aristojeff@gmail.com>
2020-10-29 19:36:04 +0900

    Update education_app.dart

--- lib/ep618_education_app/education_app.dart
+++ lib/ep618_education_app/education_app.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
@@ -30,7 +31,11 @@ class EducationSplashPage extends StatelessWidget {
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text("Education",),
+                      Text("Education",style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),),
                       Text("In Europe"),
                     ],
                   ),
