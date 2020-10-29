910cbb84811beda8bdb49fa62b7d0e3f8f3e3ce4
Dreamwalker <aristojeff@gmail.com>
2020-10-29 19:39:53 +0900

    Update education_home_page.dart

--- lib/ep618_education_app/education_home_page.dart
+++ lib/ep618_education_app/education_home_page.dart
@@ -1,2 +1,13 @@
 import 'package:flutter/material.dart';
 
+class EducationHomePage extends StatefulWidget {
+  @override
+  _EducationHomePageState createState() => _EducationHomePageState();
+}
+
+class _EducationHomePageState extends State<EducationHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
