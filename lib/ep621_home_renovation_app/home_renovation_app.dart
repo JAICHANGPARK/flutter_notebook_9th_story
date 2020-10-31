649761179cc2607b81abbe25406a5ad15f363403
Dreamwalker <aristojeff@gmail.com>
2020-10-31 17:53:25 +0900

    Update home_renovation_app.dart

--- lib/ep621_home_renovation_app/home_renovation_app.dart
+++ lib/ep621_home_renovation_app/home_renovation_app.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_9th_story/ep621_home_renovation_app/home_main_page.dart';
 
 class HomeRenovationApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: HomeMainPage(),
+    );
   }
 }
