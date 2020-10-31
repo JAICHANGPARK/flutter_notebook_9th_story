b162c9703c28f6b12feabe1d79de508c69937480
Dreamwalker <aristojeff@gmail.com>
2020-10-31 17:52:47 +0900

    Create home_main_page.dart

---
+++ lib/ep621_home_renovation_app/home_main_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class HomeMainPage extends StatefulWidget {
+  @override
+  _HomeMainPageState createState() => _HomeMainPageState();
+}
+
+class _HomeMainPageState extends State<HomeMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
