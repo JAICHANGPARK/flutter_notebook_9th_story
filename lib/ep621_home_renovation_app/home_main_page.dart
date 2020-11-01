227737a9eb969ba64a49147e8f908a1d477d014c
Dreamwalker <aristojeff@gmail.com>
2020-11-01 18:35:00 +0900

    Update home_main_page.dart

--- lib/ep621_home_renovation_app/home_main_page.dart
+++ lib/ep621_home_renovation_app/home_main_page.dart
@@ -91,6 +91,12 @@ class _HomeMainPageState extends State<HomeMainPage> {
                                                 decoration: BoxDecoration(
                                                   color: Colors.grey,
                                                   borderRadius: BorderRadius.circular(4),
+                                                  image: DecorationImage(
+                                                    image: NetworkImage(
+                                                      "https://cdn.pixabay.com/photo/2019/10/28/21/21/halloween-4585684_960_720.jpg",
+                                                    ),
+                                                    fit: BoxFit.cover
+                                                  )
                                                 ),
                                               ),
                                             )),
