68471dffb20d90feaeb63ce5a13e1f90238b7e52
Dreamwalker <aristojeff@gmail.com>
2020-11-02 19:16:30 +0900

    Update rhythm_main_page.dart

--- lib/ep623_rhythm_app/rhythm_main_page.dart
+++ lib/ep623_rhythm_app/rhythm_main_page.dart
@@ -1,1 +1,14 @@
 
+import 'package:flutter/material.dart';
+
+class RhythmMainPage extends StatefulWidget {
+  @override
+  _RhythmMainPageState createState() => _RhythmMainPageState();
+}
+
+class _RhythmMainPageState extends State<RhythmMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
