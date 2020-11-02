706ef64a29a0204029291ca43d1bba1a3bd105c3
Dreamwalker <aristojeff@gmail.com>
2020-11-02 19:16:33 +0900

    Update rhythm_app.dart

--- lib/ep623_rhythm_app/rhythm_app.dart
+++ lib/ep623_rhythm_app/rhythm_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_9th_story/ep623_rhythm_app/rhythm_main_page.dart';
 
 class RhythmApp extends StatelessWidget {
   @override
