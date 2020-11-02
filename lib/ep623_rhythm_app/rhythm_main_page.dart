37ab9af9f486c4190a55e49067348d1e3c128bff
Dreamwalker <aristojeff@gmail.com>
2020-11-02 19:19:34 +0900

    Update rhythm_main_page.dart

--- lib/ep623_rhythm_app/rhythm_main_page.dart
+++ lib/ep623_rhythm_app/rhythm_main_page.dart
@@ -13,11 +13,13 @@ class _RhythmMainPageState extends State<RhythmMainPage> {
       backgroundColor: Colors.grey[300],
       body: Stack(
         children: [
+          Positioned(child: Container()),
           Positioned(
               left: 0,
               right: 0,
               bottom: 0,
               child: Container(
+                height: 80,
                 decoration: BoxDecoration(
                   color: Colors.grey
                 ),
