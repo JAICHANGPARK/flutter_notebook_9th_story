ba863dcace103e285284d3a6d46858a6d7234b87
Dreamwalker <aristojeff@gmail.com>
2020-11-03 19:53:55 +0900

    Update rhythm_main_page.dart

--- lib/ep623_rhythm_app/rhythm_main_page.dart
+++ lib/ep623_rhythm_app/rhythm_main_page.dart
@@ -26,20 +26,23 @@ class _RhythmMainPageState extends State<RhythmMainPage> {
                       flex: 2,
                       child: Column(
                         children: [
-                          Expanded(child: Padding(
+                          Expanded(
+                              child: Padding(
                             padding: const EdgeInsets.symmetric(horizontal: 16),
                             child: Row(
                               children: [
-                                Text("EXPLORE", style: TextStyle(
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.bold
-                                ),),
+                                Text(
+                                  "EXPLORE",
+                                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                                ),
                                 Spacer(),
                                 Badge(
                                   child: Icon(Icons.notifications),
                                   badgeContent: Text("5"),
                                 ),
-                                SizedBox(width: 16,),
+                                SizedBox(
+                                  width: 16,
+                                ),
                                 Padding(
                                   padding: const EdgeInsets.all(4.0),
                                   child: CircleAvatar(
@@ -51,15 +54,18 @@ class _RhythmMainPageState extends State<RhythmMainPage> {
                           )),
                           Expanded(
                               child: Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-                                child: Container(
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(32)
+                            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(32),
+                              ),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    hintText: "Search", suffixIcon: Icon(Icons.search), border: InputBorder.none),
+                              ),
                             ),
-
-                          ),
-                              )),
+                          )),
                           Expanded(child: Placeholder()),
                         ],
                       )),
@@ -102,44 +108,47 @@ class _RhythmMainPageState extends State<RhythmMainPage> {
                             sigmaY: 5.0,
                           ),
                           child: Container(
-                            alignment: Alignment.center,
-                            padding: EdgeInsets.symmetric(horizontal: 16),
-                            child: Row(
-                              children: [
-                                Container(
-                                  height: 48,
-                                  width: 48,
-                                  decoration: BoxDecoration(
-                                    color: Colors.redAccent,
-                                    borderRadius: BorderRadius.circular(8)
+                              alignment: Alignment.center,
+                              padding: EdgeInsets.symmetric(horizontal: 16),
+                              child: Row(
+                                children: [
+                                  Container(
+                                    height: 48,
+                                    width: 48,
+                                    decoration:
+                                        BoxDecoration(color: Colors.redAccent, borderRadius: BorderRadius.circular(8)),
                                   ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(16.0),
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text("Flutter Live", style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 20
-                                      ),),
-                                      Text("Dream Walker")
-                                    ],
+                                  Padding(
+                                    padding: const EdgeInsets.all(16.0),
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          "Flutter Live",
+                                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                                        ),
+                                        Text("Dream Walker")
+                                      ],
+                                    ),
                                   ),
-                                ),
-                                Spacer(),
-                                IconButton(
-                                  icon: Icon(Icons.play_arrow, size: 32,),
-                                  onPressed: () {  },
-                                ),
-                                IconButton(
-                                  icon: Icon(Icons.skip_next,  size: 32,),
-                                  onPressed: () {  },
-                                ),
-                              ],
-                            )
-                          ),
+                                  Spacer(),
+                                  IconButton(
+                                    icon: Icon(
+                                      Icons.play_arrow,
+                                      size: 32,
+                                    ),
+                                    onPressed: () {},
+                                  ),
+                                  IconButton(
+                                    icon: Icon(
+                                      Icons.skip_next,
+                                      size: 32,
+                                    ),
+                                    onPressed: () {},
+                                  ),
+                                ],
+                              )),
                         ),
                       ),
                     ),
@@ -151,10 +160,3 @@ class _RhythmMainPageState extends State<RhythmMainPage> {
     );
   }
 }
-
-
-
-
-
-
-
