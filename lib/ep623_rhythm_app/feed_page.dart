c140ea6f65f677f9eb6d0598d45b50a211a3c7d4
Dreamwalker <aristojeff@gmail.com>
2020-11-05 19:57:59 +0900

    Update feed_page.dart

---
+++ lib/ep623_rhythm_app/feed_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FeedPage extends StatefulWidget {
+  @override
+  _FeedPageState createState() => _FeedPageState();
+}
+
+class _FeedPageState extends State<FeedPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
