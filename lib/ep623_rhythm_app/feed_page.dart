4068fd77129ba15d20415d14ccc7d002a5846ef4
Dreamwalker <aristojeff@gmail.com>
2020-11-06 20:03:35 +0900

    Update feed_page.dart

--- lib/ep623_rhythm_app/feed_page.dart
+++ lib/ep623_rhythm_app/feed_page.dart
@@ -40,9 +40,92 @@ class _FeedPageState extends State<FeedPage> {
       ),
       body: Stack(
         children: [
-
+          Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: Container(
+                height: 80,
+                child: Stack(
+                  children: [
+                    Container(
+                      color: Colors.grey[200],
+                    ),
+                    Center(
+                      child: ClipRect(
+                        // <-- clips to the 200x200 [Container] below
+                        child: BackdropFilter(
+                          filter: ui.ImageFilter.blur(
+                            sigmaX: 5.0,
+                            sigmaY: 5.0,
+                          ),
+                          child: Container(
+                              alignment: Alignment.center,
+                              padding: EdgeInsets.symmetric(horizontal: 16),
+                              child: Row(
+                                children: [
+                                  Container(
+                                    height: 48,
+                                    width: 48,
+                                    decoration:
+                                    BoxDecoration(color: Colors.redAccent, borderRadius: BorderRadius.circular(8)),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(16.0),
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          "Flutter Live",
+                                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                                        ),
+                                        Text("Dream Walker")
+                                      ],
+                                    ),
+                                  ),
+                                  Spacer(),
+                                  IconButton(
+                                    icon: Icon(
+                                      Icons.play_arrow,
+                                      size: 32,
+                                    ),
+                                    onPressed: () {},
+                                  ),
+                                  IconButton(
+                                    icon: Icon(
+                                      Icons.skip_next,
+                                      size: 32,
+                                    ),
+                                    onPressed: () {},
+                                  ),
+                                ],
+                              )),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )),
         ],
       ),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
