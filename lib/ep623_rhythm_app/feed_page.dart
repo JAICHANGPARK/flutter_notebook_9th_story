335432f6d9b4b2112f5d9b69f407a68780e179c0
Dreamwalker <aristojeff@gmail.com>
2020-11-06 19:59:23 +0900

    Update feed_page.dart

--- lib/ep623_rhythm_app/feed_page.dart
+++ lib/ep623_rhythm_app/feed_page.dart
@@ -9,7 +9,18 @@ class _FeedPageState extends State<FeedPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          child: Row(
+            children: [],
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
 
+        ],
+      ),
     );
   }
 }
