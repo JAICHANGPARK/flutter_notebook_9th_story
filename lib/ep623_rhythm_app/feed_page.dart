f17726157de83763e1c7bbd9594d6bf253d4df62
Dreamwalker <aristojeff@gmail.com>
2020-11-07 20:54:04 +0900

    Update feed_page.dart

--- lib/ep623_rhythm_app/feed_page.dart
+++ lib/ep623_rhythm_app/feed_page.dart
@@ -10,6 +10,7 @@ class _FeedPageState extends State<FeedPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       bottomNavigationBar: BottomAppBar(
         child: Container(
           height: 72,
