c78833955fee30a5d5823fe327e693ffefa4da7b
Dreamwalker <aristojeff@gmail.com>
2020-11-09 20:07:34 +0900

    Update date_profile_page.dart

--- lib/ep630_date_profile_app/date_profile_page.dart
+++ lib/ep630_date_profile_app/date_profile_page.dart
@@ -29,6 +29,13 @@ class _DateProfilePageState extends State<DateProfilePage> {
                       ),
                     ),
                   ),
+                ),
+                Positioned(
+                  left: 0,
+                  top: 0,
+                  bottom: 0,
+                  right: 0,
+                  child: Container(),
                 )
               ],
             ),
