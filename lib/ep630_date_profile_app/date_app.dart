3ae4e9c5aec1696a7dcc24b5b891136f5c0a521e
Dreamwalker <aristojeff@gmail.com>
2020-11-09 19:58:44 +0900

    Update date_app.dart

--- lib/ep630_date_profile_app/date_app.dart
+++ lib/ep630_date_profile_app/date_app.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class DateProfileApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: DateProfilePage(),
+    );
   }
 }
