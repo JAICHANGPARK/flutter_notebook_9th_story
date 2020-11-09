8bfb80a2308d1e234e68cc204426e059f4cd94d3
Dreamwalker <aristojeff@gmail.com>
2020-11-09 19:58:58 +0900

    Update date_profile_page.dart

--- lib/ep630_date_profile_app/date_profile_page.dart
+++ lib/ep630_date_profile_app/date_profile_page.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+class DateProfilePage extends StatefulWidget {
+  @override
+  _DateProfilePageState createState() => _DateProfilePageState();
+}
+
+class _DateProfilePageState extends State<DateProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
