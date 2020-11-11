8f848d7036a441f619914f80f404237146380fb9
Dreamwalker <aristojeff@gmail.com>
2020-11-11 18:34:00 +0900

    Create date_app_home_page.dart

---
+++ lib/ep630_date_profile_app/date_app_home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DateAppHomePage extends StatefulWidget {
+  @override
+  _DateAppHomePageState createState() => _DateAppHomePageState();
+}
+
+class _DateAppHomePageState extends State<DateAppHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
