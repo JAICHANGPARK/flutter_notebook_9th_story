65043e540e9dd3aebbeb5ca172b6375727bce1d6
Dreamwalker <aristojeff@gmail.com>
2020-12-10 19:16:16 +0900

    Update events_main_page.dart

--- lib/ep633_events_app/ui/events_main_page.dart
+++ lib/ep633_events_app/ui/events_main_page.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+class EventsMainPage extends StatefulWidget {
+  @override
+  _EventsMainPageState createState() => _EventsMainPageState();
+}
+
+class _EventsMainPageState extends State<EventsMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
