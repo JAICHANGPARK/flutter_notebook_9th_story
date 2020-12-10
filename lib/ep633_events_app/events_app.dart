48b91c353b8153a35b88dee859620539f4afa6ce
Dreamwalker <aristojeff@gmail.com>
2020-12-10 19:15:37 +0900

    Update events_app.dart

--- lib/ep633_events_app/events_app.dart
+++ lib/ep633_events_app/events_app.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class EventsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: EventsMainPage(),
+    );
   }
 }
