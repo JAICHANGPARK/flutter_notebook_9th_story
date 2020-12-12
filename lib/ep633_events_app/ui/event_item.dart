4eaf0d3176a658293b2ab3986d60a6e3ef352317
Dreamwalker <aristojeff@gmail.com>
2020-12-12 18:28:20 +0900

    Create event_item.dart

---
+++ lib/ep633_events_app/ui/event_item.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class EventCardItem extends StatefulWidget {
+  @override
+  _EventCardItemState createState() => _EventCardItemState();
+}
+
+class _EventCardItemState extends State<EventCardItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
