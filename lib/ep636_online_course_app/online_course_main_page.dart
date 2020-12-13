cfce3a8f2e6efabe512693fbc18abf310d110d6a
Dreamwalker <aristojeff@gmail.com>
2020-12-13 17:37:30 +0900

    Create online_course_main_page.dart

---
+++ lib/ep636_online_course_app/online_course_main_page.dart
@@ -0,0 +1,11 @@
+class OnlineCourseMainPage extends StatefulWidget {
+  @override
+  _OnlineCourseMainPageState createState() => _OnlineCourseMainPageState();
+}
+
+class _OnlineCourseMainPageState extends State<OnlineCourseMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
