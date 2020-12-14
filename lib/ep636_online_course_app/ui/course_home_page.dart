88abc9b4cbc56d10abbfd94221aba03de0d2beda
Dreamwalker <aristojeff@gmail.com>
2020-12-14 19:52:53 +0900

    Update course_home_page.dart

---
+++ lib/ep636_online_course_app/ui/course_home_page.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class CourseHomePage extends StatefulWidget {
+  @override
+  _CourseHomePageState createState() => _CourseHomePageState();
+}
+
+class _CourseHomePageState extends State<CourseHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
