5a8c1edfdfd74be0dc4e3e682a226c725c1b9db2
Dreamwalker <aristojeff@gmail.com>
2020-12-14 19:57:47 +0900

    Update course_home_page.dart

--- lib/ep636_online_course_app/ui/course_home_page.dart
+++ lib/ep636_online_course_app/ui/course_home_page.dart
@@ -11,42 +11,49 @@ class _CourseHomePageState extends State<CourseHomePage> {
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Class Previews", style: TextStyle(
-              color: Colors.white
-            ),),
-            Container(
-              height: 220,
-              color: Colors.blueAccent,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
+        Padding(
+          padding: const EdgeInsets.only(left: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Class Previews", style: TextStyle(
+                color: Colors.white
+              ),),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: Container(
+                  height: 220,
+                  color: Colors.blueAccent,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
                     children: [
-                      Container(
-                        height: 160,
-                        width: 240,
-                        decoration: BoxDecoration(
-                          color: Colors.redAccent,
-                        ),
-                      ),
-                      Text("Dream Walker", style: TextStyle(
-                        color: Colors.white
-                      ),),
-                      Divider(),
-                      Text("Flutter Live Coding", style: TextStyle(
-                        color: Colors.grey
-                      ),)
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Container(
+                            height: 160,
+                            width: 240,
+                            decoration: BoxDecoration(
+                              color: Colors.redAccent,
+                            ),
+                          ),
+                          Text("Dream Walker", style: TextStyle(
+                            color: Colors.white
+                          ),),
+                          Divider(),
+                          Text("Flutter Live Coding", style: TextStyle(
+                            color: Colors.grey
+                          ),)
+                        ],
+                      )
                     ],
-                  )
-                ],
-              ),
-            )
-          ],
+                  ),
+                ),
+              )
+            ],
+          ),
         )
       ],
     );
