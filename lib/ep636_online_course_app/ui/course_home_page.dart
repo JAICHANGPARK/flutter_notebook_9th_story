c8e7fa8554e0de2711a2e43d16ec415ff2a2deed
Dreamwalker <aristojeff@gmail.com>
2020-12-14 20:00:53 +0900

    Update course_home_page.dart

--- lib/ep636_online_course_app/ui/course_home_page.dart
+++ lib/ep636_online_course_app/ui/course_home_page.dart
@@ -56,6 +56,38 @@ class _CourseHomePageState extends State<CourseHomePage> {
                             style: TextStyle(color: Colors.grey),
                           )
                         ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 16),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Container(
+                              height: 160,
+                              width: 240,
+                              decoration: BoxDecoration(
+                                  color: Colors.redAccent,
+                                  borderRadius: BorderRadius.circular(8)
+                              ),
+                            ),
+                            SizedBox(
+                              height: 16,
+                            ),
+                            Text(
+                              "Dream Walker",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 8),
+                              child: Container(height: 1, width: 100, decoration: BoxDecoration(color: Colors.grey)),
+                            ),
+                            Text(
+                              "Flutter Live Coding",
+                              style: TextStyle(color: Colors.grey),
+                            )
+                          ],
+                        ),
                       )
                     ],
                   ),
