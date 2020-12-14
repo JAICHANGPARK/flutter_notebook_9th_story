8603c90c816dbd0f957707ce76bf57539a7627c3
Dreamwalker <aristojeff@gmail.com>
2020-12-14 19:57:11 +0900

    Update course_home_page.dart

--- lib/ep636_online_course_app/ui/course_home_page.dart
+++ lib/ep636_online_course_app/ui/course_home_page.dart
@@ -17,30 +17,34 @@ class _CourseHomePageState extends State<CourseHomePage> {
             Text("Class Previews", style: TextStyle(
               color: Colors.white
             ),),
-            ListView(
-              scrollDirection: Axis.horizontal,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Container(
-                      height: 160,
-                      width: 240,
-                      decoration: BoxDecoration(
-                        color: Colors.redAccent,
+            Container(
+              height: 220,
+              color: Colors.blueAccent,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                        height: 160,
+                        width: 240,
+                        decoration: BoxDecoration(
+                          color: Colors.redAccent,
+                        ),
                       ),
-                    ),
-                    Text("Dream Walker", style: TextStyle(
-                      color: Colors.white
-                    ),),
-                    Divider(),
-                    Text("Flutter Live Coding", style: TextStyle(
-                      color: Colors.grey
-                    ),)
-                  ],
-                )
-              ],
+                      Text("Dream Walker", style: TextStyle(
+                        color: Colors.white
+                      ),),
+                      Divider(),
+                      Text("Flutter Live Coding", style: TextStyle(
+                        color: Colors.grey
+                      ),)
+                    ],
+                  )
+                ],
+              ),
             )
           ],
         )
