18caea75d8f85e729366c84a83e09aff18a9cf87
Dreamwalker <aristojeff@gmail.com>
2020-12-16 19:37:32 +0900

    Update fashion_app.dart

--- lib/ep639_fashion_app/fashion_app.dart
+++ lib/ep639_fashion_app/fashion_app.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 class FashionApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: FashionStartPage(),
+    );
   }
 }
