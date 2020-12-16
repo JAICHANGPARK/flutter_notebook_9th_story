7db6fabdf2eae6a1e613682106972e3f68b03f9b
Dreamwalker <aristojeff@gmail.com>
2020-12-16 19:38:56 +0900

    Update start_page.dart

--- lib/ep639_fashion_app/ui/start_page.dart
+++ lib/ep639_fashion_app/ui/start_page.dart
@@ -1,3 +1,14 @@
 import 'package:flutter/material.dart';
 
 
+class FashionStartPage extends StatefulWidget {
+  @override
+  _FashionStartPageState createState() => _FashionStartPageState();
+}
+
+class _FashionStartPageState extends State<FashionStartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
