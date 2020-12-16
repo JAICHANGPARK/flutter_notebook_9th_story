4651a139ae8a73c086b0e4f4cc8cd8322da4a4a1
Dreamwalker <aristojeff@gmail.com>
2020-12-16 19:37:54 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'ep636_online_course_app/online_course_app.dart';
+import 'ep639_fashion_app/fashion_app.dart';
 
 main() {
-  runApp(OnlineCourseApp());
+  runApp(FashionApp());
 }
