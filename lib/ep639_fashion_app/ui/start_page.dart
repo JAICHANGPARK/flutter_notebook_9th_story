b855cbe8602215317df642f122ae2dd7fa30cec3
Dreamwalker <aristojeff@gmail.com>
2020-12-16 19:42:18 +0900

    Update start_page.dart

--- lib/ep639_fashion_app/ui/start_page.dart
+++ lib/ep639_fashion_app/ui/start_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class FashionStartPage extends StatefulWidget {
   @override
   _FashionStartPageState createState() => _FashionStartPageState();
@@ -13,18 +12,18 @@ class _FashionStartPageState extends State<FashionStartPage> {
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
-        iconTheme: IconThemeData(
-            color: Colors.black
-        ),
+        iconTheme: IconThemeData(color: Colors.black),
         leading: Row(
           children: [
             Icon(Icons.keyboard_arrow_down_rounded),
-            Text("ENG", style: TextStyle(
-              color: Colors.black,
-            ),)
+            Text(
+              "ENG",
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            )
           ],
-      ),
-
+        ),
       ),
     );
   }
