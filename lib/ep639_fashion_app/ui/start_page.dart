068ea41a0ddead3b97651c69315da43342e174a1
Dreamwalker <aristojeff@gmail.com>
2020-12-17 19:34:17 +0900

    Update start_page.dart

--- lib/ep639_fashion_app/ui/start_page.dart
+++ lib/ep639_fashion_app/ui/start_page.dart
@@ -43,8 +43,9 @@ class _FashionStartPageState extends State<FashionStartPage> {
           Positioned(left: 0, right: 0, bottom: 0, top: 0, child: Container(
             decoration: BoxDecoration(
               image: DecorationImage(
-                image: NetworkImage("")
-              )
+                image: NetworkImage("https://cdn.pixabay.com/photo/2016/04/16/19/51/girl-1333640_960_720.jpg"),
+                fit: BoxFit.cover
+              ),
             ),
           )),
         ],
