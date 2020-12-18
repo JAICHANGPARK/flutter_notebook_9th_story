8c090863cf7620fef597e38d048866de7f5a7c7c
Dreamwalker <aristojeff@gmail.com>
2020-12-18 20:01:07 +0900

    Update style_theory_app.dart

---
+++ lib/ep641_style_app/style_theory_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class StyleTheoryApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: StyleMainPage(),
+    );
+  }
+}
