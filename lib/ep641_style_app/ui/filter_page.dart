cca14b7f7f822eee63b33383ca2a769bd175b8f0
Dreamwalker <aristojeff@gmail.com>
2020-12-22 20:17:06 +0900

    Create filter_page.dart

---
+++ lib/ep641_style_app/ui/filter_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class  extends StatefulWidget {
+  @override
+  _FilterPageState createState() => _FilterPageState();
+}
+
+class _FilterPageState extends State<FilterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
