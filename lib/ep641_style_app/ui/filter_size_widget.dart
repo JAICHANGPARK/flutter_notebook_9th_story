4a834d5fcc9d582e88fcff99b2a5d3b74be694ba
Dreamwalker <aristojeff@gmail.com>
2020-12-23 17:05:40 +0900

    Update filter_size_widget.dart

--- lib/ep641_style_app/ui/filter_size_widget.dart
+++ lib/ep641_style_app/ui/filter_size_widget.dart
@@ -4,6 +4,26 @@ import 'package:flutter/material.dart';
 class FilterSizeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return  InkWell(
+      onTap: (){
+        setState(() {
+          _sizeSelected = 0;
+        });
+      },
+      child: Container(
+        height: 42,
+        width: 42,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          border: Border.all(color: Colors.grey),
+        ),
+        child: Center(
+          child: Text(
+            "XXS",
+            style: TextStyle(color: Colors.grey),
+          ),
+        ),
+      ),
+    );
   }
 }
