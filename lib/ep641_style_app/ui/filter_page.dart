b038129f8c2ed715e61b6077927e375876a3828a
Dreamwalker <aristojeff@gmail.com>
2020-12-23 17:08:51 +0900

    Update filter_page.dart

--- lib/ep641_style_app/ui/filter_page.dart
+++ lib/ep641_style_app/ui/filter_page.dart
@@ -146,17 +146,24 @@ class _StyleFilterPageState extends State<StyleFilterPage> {
                         ),
                         Padding(
                           padding: const EdgeInsets.symmetric(horizontal: 4),
-                          child: Container(
-                            height: 42,
-                            width: 42,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              border: Border.all(color: Colors.grey),
-                            ),
-                            child: Center(
-                              child: Text("L", style: TextStyle(
-                                  color: Colors.grey
-                              ),),
+                          child: InkWell(
+                            onTap: (){
+                              setState(() {
+                                _sizeSelected = 4;
+                              });
+                            },
+                            child: Container(
+                              height: 42,
+                              width: 42,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border: Border.all(color: Colors.grey),
+                              ),
+                              child: Center(
+                                child: Text("L", style: TextStyle(
+                                    color: Colors.grey
+                                ),),
+                              ),
                             ),
                           ),
                         ),
