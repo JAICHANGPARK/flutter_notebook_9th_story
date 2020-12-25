87f5e1d340b2b4f13f01b28f4bd2267b3ccc796a
Dreamwalker <aristojeff@gmail.com>
2020-12-25 17:23:45 +0900

    Update workout_plans_app.dart

--- lib/ep648_workout_plans_app/workout_plans_app.dart
+++ lib/ep648_workout_plans_app/workout_plans_app.dart
@@ -13,3 +13,24 @@ class WorkoutPlansApp extends StatelessWidget {
     );
   }
 }
+
+
+class WorkoutEnterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class WorkoutMainPage extends StatefulWidget {
+  @override
+  _WorkoutMainPageState createState() => _WorkoutMainPageState();
+}
+
+class _WorkoutMainPageState extends State<WorkoutMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
