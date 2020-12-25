a0e0f42b6921e090432ac301a4a3ed8986034d85
Dreamwalker <aristojeff@gmail.com>
2020-12-25 17:23:53 +0900

    Update workout_plans_app.dart

--- lib/ep648_workout_plans_app/workout_plans_app.dart
+++ lib/ep648_workout_plans_app/workout_plans_app.dart
@@ -5,6 +5,7 @@ class WorkoutPlansApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: '/',
       routes: {
         "/" : (context) => WorkoutEnterPage(),
         "/main" : (context) => WorkoutMainPage(),
